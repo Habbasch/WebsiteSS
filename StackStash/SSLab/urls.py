94c8a236a427f118f29e5d8fe6da8f777ff5fa2a
Habbasch <fish.grepper@gmail.com>
2017-06-15 20:53:46 +0200

    Several updates : Django Tutorial page 3

--- StackStash/SSLab/urls.py
+++ StackStash/SSLab/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	# ex: /SSLab/
+	url(r'^$', views.index, name='index'),
+	# ex: /SSLab/5/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	# ex: /SSLab/5/results
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	# ex: /SSLab/5/vote
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	
 ]
--- StackStash/SSLab/views.py
+++ StackStash/SSLab/views.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World.")
+	#orders all question in descending (newest first) order and gets the first 5
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list':latest_question_list,
+	}
+
+	return render(request, 'SSLab/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'SSLab/detail.html', {'question': question})
+
+def results(request, question_id):
+	response = "You are looking at the results of question %s."
+	return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on question %s." %question_id)
